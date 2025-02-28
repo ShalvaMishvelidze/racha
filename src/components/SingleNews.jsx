b55d16f0fec633b15bf3f77a9a57adc17994ce54
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-02-28 12:15:00 +0400

    finished news page mobile version

--- src/components/SingleNews.jsx
+++ src/components/SingleNews.jsx
@@ -2,16 +2,24 @@ import { office } from "../assets";
 
 const SingleNews = () => {
   return (
-    <div className="w-[785px]">
-      <h1 className="mb-[24px] text-[30px] text-blue-primary leading-[38px] tracking-[1.5px] text-primary-blue opacity-90 font-bold">
+    <div className="w-[785px] max-sm:w-full ">
+      <h1
+        className="mb-[24px] text-[30px] max-sm:text-[16px] 
+      text-blue-primary leading-[38px] max-sm:leading-[20px] 
+      tracking-[1.5px] text-primary-blue opacity-90 font-bold"
+      >
         სიახლეები
       </h1>
       <img
-        className="mb-[24px] w-full h-[502px] rounded-[30px] object-cover"
+        className="mb-[24px] w-full h-[502px] max-sm:h-[265px] 
+        rounded-[30px] max-sm:rounded-[10px] object-cover"
         src={office}
         alt="office"
       />
-      <p className="text-blue-primary text-[18px] leading-[26px] opacity-60">
+      <p
+        className="text-blue-primary text-[18px] max-sm:text-[12px] 
+      leading-[26px] max-sm:leading-[16px] opacity-60"
+      >
         Racha with Love დაგეხმარება დაგეგმო შენი მოგზაურობა რაჭაში და დაუვიწყარი
         თავგადასავლის
         <br /> მონაწილე გახდე. ვენთან ერთად შეძლებ დაჯავშნო სასტუმროები, დაგეგმო
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,18 +4,26 @@ import SingleNews from "../components/SingleNews";
 
 const News = () => {
   return (
-    <main className="bg-primary pb-[200px]">
+    <main className="bg-primary pb-[200px] max-sm:pb-[95px]">
       <HeroBanner heading={"სიახლეები"} />
-      <p className="mx-25 mt-[115px] w-[70%] text-[30px] leading-[40px] text-blue-primary opacity-60">
+      <p
+        className="mx-25 max-sm:mx-0 max-sm:px-5 
+      mt-[115px] max-sm:mt-[32px] w-[70%] max-sm:w-full 
+      text-[30px] max-sm:text-[12px] leading-[40px] max-sm:leading-[16px] 
+      text-blue-primary opacity-60"
+      >
         სტარტაპის შექმნის იდეა ონის სკოლის 4 მოსწავლეს ეკუთვნის, რომელთა მიზანიც
         რეგიონალური ტურიზმის განვითარების ხელშეწყობა და რეგიონალურ მდგრად
         განვითარებაში მონაწილეობის მიღებაა.
       </p>
-      <div className="flex gap-[122px] mx-25 mt-[100px]">
+      <div
+        className="flex gap-[122px] max-sm:gap-[50px] max-sm:flex-col
+      mx-25 max-sm:mx-5 mt-[100px] max-sm:mt-[50px]"
+      >
         <SingleNews />
         <SingleNews />
       </div>
-      <div className="mx-25">
+      <div className="mx-25 max-sm:mx-5 max-sm:mt-[50px]">
         <Pagination />
       </div>
     </main>
