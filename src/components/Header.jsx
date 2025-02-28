612d4bcaaa9b1f429522ad6a5eb1cc27fe18be58
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-02-28 16:59:20 +0400

    finished register mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,80 +30,82 @@ const Header = () => {
   }, [width]);
 
   return (
-    <header
-      className="absolute w-full top-0 left-0 flex justify-between items-start 
+    <>
+      <header
+        className="absolute w-full top-0 left-0 flex justify-between items-start 
     px-25 max-sm:px-5 py-12 max-sm:py-5 z-100"
-    >
-      <div className="flex items-start gap-25">
-        <div
-          className="w-[147px] max-sm:w-[50px] h-[135px] max-sm:h-[40px]
+      >
+        <div className="flex items-start gap-25">
+          <div
+            className="w-[147px] max-sm:w-[50px] h-[135px] max-sm:h-[40px]
            bg-red-80 rounded-tl-3xl max-sm:rounded-tl-[5px] 
            rounded-br-3xl max-sm:rounded-br-[5px] 
            rounded-tr-lg rounded-tr-[15px] 
            rounded-bl-lg rounded-bl-[15px] 
         flex flex-col justify-center items-center 
         cursor-pointer"
-        >
-          <PiHeartStraightLight className="text-[#ffffff] text-[90px] max-sm:text-[32px]" />
-          <h1 className="text-2xl max-sm:text-[5px] font-bold max-sm:font-normal text-[#ffffff]">
-            With Love
-          </h1>
-          <span className="text-[8px] max-sm:text-[2px] text-[#ffffff] mb-1">
-            RACHA
-          </span>
+          >
+            <PiHeartStraightLight className="text-[#ffffff] text-[90px] max-sm:text-[32px]" />
+            <h1 className="text-2xl max-sm:text-[5px] font-bold max-sm:font-normal text-[#ffffff]">
+              With Love
+            </h1>
+            <span className="text-[8px] max-sm:text-[2px] text-[#ffffff] mb-1">
+              RACHA
+            </span>
+          </div>
+          <Nav />
         </div>
-        <Nav />
-      </div>
-      <div className="flex gap-6 max-sm:gap-[22px] justify-center items-center">
-        {!localUser && (
-          <div className="max-sm:hidden">
-            <AuthBtns />
-          </div>
-        )}
-        {!localUser && (
-          <div className="hidden max-sm:block">
-            <ThemeBtn />
-          </div>
-        )}
-        {localUser && (
-          <div className="flex justify-center items-center gap-[44px]  max-sm:gap-[12px]">
-            <h2 className="text-blue-primary max-sm:text-[13px] max-sm:leading-[38px] font-bold">
-              GIORGI 99
-            </h2>
-            <div className="relative max-sm:size-[38px] w-14 h-14 cursor-pointer">
-              <FaUser className="max-sm:text-[18px] text-[24px] text-[#ffffff] absolute top-1/2 left-1/2 transform -translate-1/2 z-10" />
-              <img
-                src={userBcg}
-                alt="localUser background"
-                className="absolute top-0 left-0 w-full h full"
-              />
+        <div className="flex gap-6 max-sm:gap-[22px] justify-center items-center">
+          {!localUser && (
+            <div className="max-sm:hidden">
+              <AuthBtns />
             </div>
-          </div>
-        )}
-        <div className="max-sm:size-[38px] relative border-[2px] border-blue-primary flex justify-center items-center w-14 h-14 cursor-pointer rounded-full">
-          <span className="max-sm:text-[14px] leading-[19px] text-blue-primary font-bold">
-            Eng
-          </span>
-        </div>
-        <button
-          className="hidden max-sm:block cursor-pointer"
-          type="button"
-          onClick={() => setAside(!aside)}
-        >
-          {aside ? (
-            <FaXmark className="text-[32px] text-blue-primary" />
-          ) : (
-            <HiOutlineMenuAlt3 className="text-[32px] text-blue-primary" />
           )}
-        </button>
-      </div>
-      {aside && <Aside />}
+          {!localUser && (
+            <div className="hidden max-sm:block">
+              <ThemeBtn />
+            </div>
+          )}
+          {localUser && (
+            <div className="flex justify-center items-center gap-[44px]  max-sm:gap-[12px]">
+              <h2 className="text-blue-primary max-sm:text-[13px] max-sm:leading-[38px] font-bold">
+                GIORGI 99
+              </h2>
+              <div className="relative max-sm:size-[38px] w-14 h-14 cursor-pointer">
+                <FaUser className="max-sm:text-[18px] text-[24px] text-[#ffffff] absolute top-1/2 left-1/2 transform -translate-1/2 z-10" />
+                <img
+                  src={userBcg}
+                  alt="localUser background"
+                  className="absolute top-0 left-0 w-full h full"
+                />
+              </div>
+            </div>
+          )}
+          <div className="max-sm:size-[38px] relative border-[2px] border-blue-primary flex justify-center items-center w-14 h-14 cursor-pointer rounded-full">
+            <span className="max-sm:text-[14px] leading-[19px] text-blue-primary font-bold">
+              Eng
+            </span>
+          </div>
+          <button
+            className="hidden max-sm:block cursor-pointer"
+            type="button"
+            onClick={() => setAside(!aside)}
+          >
+            {aside ? (
+              <FaXmark className="text-[32px] text-blue-primary" />
+            ) : (
+              <HiOutlineMenuAlt3 className="text-[32px] text-blue-primary" />
+            )}
+          </button>
+        </div>
+        {aside && <Aside />}
+      </header>
       {!localUser && (
-        <div className="hidden max-sm:block">
+        <div className="hidden max-sm:block relative z-1000">
           <AuthBtns />
         </div>
       )}
-    </header>
+    </>
   );
 };
 
--- src/components/InputDesktop.jsx
+++ src/components/InputDesktop.jsx
@@ -3,7 +3,7 @@ const InputDesktop = ({ label, name, type, value, user, setUser }) => {
     <div className="flex flex-col ">
       <label
         className="text-[18px] leading-[40px] 
-      text-blue-90 opacity-90 font-bold"
+      text-blue-90 opacity-90 font-bold max-sm:hidden"
         htmlFor={name}
       >
         {label}
@@ -11,8 +11,12 @@ const InputDesktop = ({ label, name, type, value, user, setUser }) => {
       <input
         type={type}
         name={name}
+        placeholder={label}
         id={name}
-        className="outline-none border-b border-[#000000] h-[12px]"
+        className="outline-none border-b max-sm:border-b-0 
+        border-[#000000] h-[12px] max-sm:h-[49px] max-sm:rounded-[50px]
+        max-sm:bg-primary max-sm:pl-[22px] max-sm:placeholder-blue-primary 
+        max-sm:text-[13px] max-sm:leading-[40px] max-sm:font-bold"
         value={value}
         onChange={(e) => setUser({ ...user, [name]: e.target.value })}
       />
