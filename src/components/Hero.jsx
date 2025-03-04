a543f12f4add1c7c8cc1d665937c2deac4243f3c
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-03-04 14:34:28 +0400

    made hero responsive for up to 1600px screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,37 +5,46 @@ import ThemeBtn from "./ThemeBtn";
 const Hero = () => {
   const [user] = useLocalStorage("user", null);
   return (
-    <section className="relative z-1 h-[710px] max-sm:h-auto pt-[142px] max-sm:pt-[156px] mx-25 max-sm:mx-5 flex flex-col justify-center">
+    <section className="relative z-1 h-[710px] max-xxl:h-auto pt-[142px] max-xxl:pt-[156px] mx-25 max-xxl:mx-5 flex flex-col justify-center">
       <Slider />
-      <div className="z-20 flex flex-col max-sm:h-full max-sm:w-full gap-10 max-sm:absolute top-0 left-0">
+      <div className="z-20 flex flex-col max-xxl:h-full max-xxl:w-full gap-10 max-xxl:absolute top-0 left-0">
         <h1
-          className="max-w-[983px] max-sm:w-[65%] max-sm:text-[20px] text-[60px] 
-        leading-[38px] max-sm:leading-[24px] 
+          className="max-w-[983px] max-xxl:w-[65%] max-lg:text-[20px] text-[60px] max-xxl:text-[40px]
+        leading-[38px] max-xxl:leading-[24px] 
         text-blue-primary font-bold 
-        max-sm:absolute left-[30px] bottom-[100px]"
+        max-xxl:absolute left-[30px] bottom-[100px]"
         >
           რაჭა ჩვენი სიყვარულია
         </h1>
         <p
-          className="max-w-[983px] max-sm:w-[55%] 
-        text-[20px] max-sm:text-[10px] leading-[25px] max-sm:leading-[16px] 
-        text-blue-primary font-bold max-sm:font-normal opacity-80 
-        max-sm:absolute left-[30px] bottom-[24px]"
+          className="max-w-[983px] max-xxl:w-[55%] 
+        text-[20px] max-xxl:text-[16px] max-lg:text-[10px] leading-[25px] max-xxl:leading-[16px] 
+        text-blue-primary font-bold max-xxl:font-normal opacity-80 
+        max-xxl:absolute left-[30px] bottom-[24px]"
         >
           ჩვენ ვაერთიანებთ რეგიონული მასშტაბის ტურისტულ ობიექტებს, სერვისებსა და
           ღირსშესანიშნაობებს.
         </p>
+        <div className="absolute left-[30px] bottom-[200px] gap-[21px] items-center z-100 max-lg:hidden hidden max-xxl:flex">
+          <h1
+            className="leading-[32px] text-blue-primary font-bold 
+        max-xxl:text-[12px] max-xxl:leading-[32px] max-xxl:font-normal"
+          >
+            DARK MODE
+          </h1>
+          <ThemeBtn />
+        </div>
       </div>
       {user && (
         <div
           className="absolute left-0 bottom-0 
-      max-sm:right-0 max-sm:top-[80px] max-sm:left-auto
-       max-sm:bottom-auto flex gap-[21px] 
-       max-sm:gap-[7px] items-center z-100"
+      max-xxl:right-0 max-xxl:top-[80px] max-xxl:left-auto
+       max-xxl:bottom-auto flex gap-[21px] 
+       max-xxl:gap-[7px] items-center z-100"
         >
           <h1
             className="leading-[32px] text-blue-primary font-bold 
-        max-sm:text-[12px] max-sm:leading-[32px] max-sm:font-normal"
+        max-xxl:text-[12px] max-xxl:leading-[32px] max-xxl:font-normal"
           >
             DARK MODE
           </h1>
@@ -44,13 +53,13 @@ const Hero = () => {
       )}
       <div
         className="absolute left-0 bottom-0 
-      max-sm:right-0 max-sm:top-[80px] max-sm:left-auto
-       max-sm:bottom-auto flex gap-[21px] 
-       max-sm:gap-[7px] items-center z-100 max-sm:hidden "
+      max-xxl:right-0 max-xxl:top-[80px] max-xxl:left-auto
+       max-xxl:bottom-auto flex gap-[21px] 
+       max-xxl:gap-[7px] items-center z-100 max-xxl:hidden "
       >
         <h1
           className="leading-[32px] text-blue-primary font-bold 
-        max-sm:text-[12px] max-sm:leading-[32px] max-sm:font-normal"
+        max-xxl:text-[12px] max-xxl:leading-[32px] max-xxl:font-normal"
         >
           DARK MODE
         </h1>
@@ -58,7 +67,7 @@ const Hero = () => {
       </div>
       <h2
         className="text-[20px] text-blue-primary rotate-90 w-auto 
-      absolute -right-27 bottom-20 z-100 max-sm:hidden"
+      absolute -right-27 bottom-20 z-100 max-xxl:hidden"
       >
         Racha with love 2022
       </h2>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,8 +5,8 @@ const Slider = () => {
   return (
     <>
       <div
-        className="z-10 w-[1265px] max-sm:w-full  h-[568px] max-sm:h-[274px]
-    rounded-[30px] absolute max-sm:relative right-[67px] max-sm:right-0 z-10 overflow-hidden"
+        className="z-10 w-[1265px] max-xxl:w-full  h-[568px] max-xxl:h-[274px]
+    rounded-[30px] absolute max-xxl:relative right-[67px] max-xxl:right-0 z-10 overflow-hidden"
       >
         <img
           className="w-full h-full object-cover opacity-65"
@@ -15,24 +15,24 @@ const Slider = () => {
         />
       </div>
       <div
-        className="absolute right-[110px] max-sm:right-[26px] 
-      bottom-[50px] max-sm:bottom-[28px] flex gap-[35px] 
-      max-sm:gap-[20px] z-50"
+        className="absolute right-[110px] max-xxl:right-[26px] 
+      bottom-[50px] max-xxl:bottom-[28px] flex gap-[35px] 
+      max-xxl:gap-[20px] z-50"
       >
         <button
-          className="group size-12 max-sm:size-[32px] border border-[#ffffff] rounded-full 
+          className="group size-12 max-xxl:size-[32px] border border-[#ffffff] rounded-full 
         flex justify-center items-center hover:bg-[#ffffff] cursor-pointer
         "
           type="button"
         >
-          <BsArrowLeft className="text-[#ffffff] text-[24px] max-sm:text-[18px] group-hover:text-[#000000] " />
+          <BsArrowLeft className="text-[#ffffff] text-[24px] max-xxl:text-[18px] group-hover:text-[#000000] " />
         </button>
         <button
           type="button"
-          className="group size-12 max-sm:size-[32px] border border-[#ffffff] rounded-full 
+          className="group size-12 max-xxl:size-[32px] border border-[#ffffff] rounded-full 
         flex justify-center items-center hover:bg-[#ffffff] cursor-pointer"
         >
-          <BsArrowRight className="text-[#ffffff] text-[24px] max-sm:text-[18px] group-hover:text-[#000000] " />
+          <BsArrowRight className="text-[#ffffff] text-[24px] max-xxl:text-[18px] group-hover:text-[#000000] " />
         </button>
       </div>
     </>
